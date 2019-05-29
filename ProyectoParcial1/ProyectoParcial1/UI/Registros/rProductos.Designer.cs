1ae242047e07586141e6fce4cafa4da02d23f785
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-05-29 10:19:58 -0400

    Diseñando

--- ProyectoParcial1/ProyectoParcial1/UI/Registros/rProductos.Designer.cs
+++ ProyectoParcial1/ProyectoParcial1/UI/Registros/rProductos.Designer.cs
@@ -28,19 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rProductos));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProductoId_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Descripcion_textBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Buscar_button = new System.Windows.Forms.Button();
+            this.Eliminar_button = new System.Windows.Forms.Button();
+            this.Guardar_button = new System.Windows.Forms.Button();
+            this.Nuevo_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoId_numericUpDown)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ProductoId";
+            // 
+            // ProductoId_numericUpDown
+            // 
+            this.ProductoId_numericUpDown.Location = new System.Drawing.Point(179, 44);
+            this.ProductoId_numericUpDown.Name = "ProductoId_numericUpDown";
+            this.ProductoId_numericUpDown.Size = new System.Drawing.Size(181, 20);
+            this.ProductoId_numericUpDown.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Existencia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Costo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Descripcion";
+            // 
+            // Descripcion_textBox
+            // 
+            this.Descripcion_textBox.Location = new System.Drawing.Point(179, 90);
+            this.Descripcion_textBox.Name = "Descripcion_textBox";
+            this.Descripcion_textBox.Size = new System.Drawing.Size(294, 20);
+            this.Descripcion_textBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Valor Inventario";
+            // 
+            // Buscar_button
+            // 
+            this.Buscar_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscar_button.Image = global::ProyectoParcial1.Properties.Resources.icono;
+            this.Buscar_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Buscar_button.Location = new System.Drawing.Point(373, 26);
+            this.Buscar_button.Name = "Buscar_button";
+            this.Buscar_button.Size = new System.Drawing.Size(100, 38);
+            this.Buscar_button.TabIndex = 13;
+            this.Buscar_button.Text = "Buscar";
+            this.Buscar_button.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Buscar_button.UseVisualStyleBackColor = true;
+            // 
+            // Eliminar_button
+            // 
+            this.Eliminar_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eliminar_button.Image = global::ProyectoParcial1.Properties.Resources.delete_icon;
+            this.Eliminar_button.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Eliminar_button.Location = new System.Drawing.Point(331, 316);
+            this.Eliminar_button.Name = "Eliminar_button";
+            this.Eliminar_button.Size = new System.Drawing.Size(100, 64);
+            this.Eliminar_button.TabIndex = 12;
+            this.Eliminar_button.Text = "Eliminar";
+            this.Eliminar_button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Eliminar_button.UseVisualStyleBackColor = true;
+            // 
+            // Guardar_button
+            // 
+            this.Guardar_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Guardar_button.Image = global::ProyectoParcial1.Properties.Resources.Save_black_512_icon;
+            this.Guardar_button.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Guardar_button.Location = new System.Drawing.Point(202, 316);
+            this.Guardar_button.Name = "Guardar_button";
+            this.Guardar_button.Size = new System.Drawing.Size(100, 64);
+            this.Guardar_button.TabIndex = 11;
+            this.Guardar_button.Text = "Guardar";
+            this.Guardar_button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Guardar_button.UseVisualStyleBackColor = true;
+            // 
+            // Nuevo_button
+            // 
+            this.Nuevo_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nuevo_button.Image = global::ProyectoParcial1.Properties.Resources.application_add_512_icon;
+            this.Nuevo_button.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Nuevo_button.Location = new System.Drawing.Point(71, 316);
+            this.Nuevo_button.Name = "Nuevo_button";
+            this.Nuevo_button.Size = new System.Drawing.Size(100, 64);
+            this.Nuevo_button.TabIndex = 10;
+            this.Nuevo_button.Text = "Nuevo";
+            this.Nuevo_button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Nuevo_button.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 16;
             // 
             // rProductos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 404);
+            this.ClientSize = new System.Drawing.Size(517, 400);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Buscar_button);
+            this.Controls.Add(this.Eliminar_button);
+            this.Controls.Add(this.Guardar_button);
+            this.Controls.Add(this.Nuevo_button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Descripcion_textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ProductoId_numericUpDown);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "rProductos";
-            this.Text = "rProductos";
+            this.Text = "Registro de Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.ProductoId_numericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown ProductoId_numericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Descripcion_textBox;
+        private System.Windows.Forms.Button Nuevo_button;
+        private System.Windows.Forms.Button Guardar_button;
+        private System.Windows.Forms.Button Eliminar_button;
+        private System.Windows.Forms.Button Buscar_button;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
